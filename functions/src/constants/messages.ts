7f5d0f74f6b18fe983b73cd063d472d90e3176f4
Tulio Fernandes <tfernandes@qmo.io>
2022-06-18 10:25:31 -0400

    Extract common text into messages constants

---
+++ functions/src/constants/messages.ts
@@ -0,0 +1,10 @@
+export const messages = {
+  start: ":large_green_square: New game of wordle available! :large_green_square:",
+  end: ":large_green_square: Game of wordle ended! :large_green_square:",
+  invalid: "🤔 Invalid guess, please try again!",
+  correct: "You guessed right! Congratulations 🏆",
+  wrong: "You lose 😵 The word was wordOfTheDay",
+  outOfGuesses: "You are out of guesses!",
+  gameOverLost: "👾 Game Over, see you tomorrow! 👋",
+  gameOverWin: "🏆 You did it, see you tomorrow champ! 👋",
+};
